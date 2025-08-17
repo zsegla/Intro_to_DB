e4c1aad9bf05b3e924d22011cb6dbf489e082e7a
Fekreselassie <fekruson@gmail.com>
2025-08-17 13:20:38 +0300

    update task 6

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,11 @@
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+INSERT INTO customer (
+    customer_id,
+    customer_name,
+    email,
+    address
+) VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
